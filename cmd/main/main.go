35021034ba45c1d229b8502d368c5af4780bb14b
OddEer0 <oddeer0@gmail.com>
2024-04-25 16:51:21 +0300

    [feature/86bygn3r9]: add config

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"github.com/OddEer0/mirage-auth-service/internal/infrastructure/config"
+	"github.com/go-chi/chi/v5"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello")
+	cfg := config.MustLoad()
+	router := chi.NewRouter()
+	router.Get("/", func(res http.ResponseWriter, req *http.Request) {
+		_, _ = res.Write([]byte("Hello"))
+	})
+	err := http.ListenAndServe(cfg.Server.Address, router)
+	if err != nil {
+		return
+	}
 }
