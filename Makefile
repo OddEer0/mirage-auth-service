6903fb0972eeddd986fee44762a50fa672176999
OddEer0 <oddeer0@gmail.com>
2024-04-29 21:36:54 +0300

    [feature/86bygacq8]: add grpc proto v1

--- Makefile
+++ Makefile
@@ -1,5 +1,13 @@
+PROTO_FILES=api/auth_v1/*.proto
+PROTO_SRC=api
+PROTO_OUT=./pkg/gen
+PROTO_GRPC_OUT=./internal/gen/go/
+
 dev:
 	CONFIG_PATH=./config/dev.yaml go run cmd/main/main.go
 
 build:
 	go build cmd/main/main.go
+
+proto:
+	protoc -I $(PROTO_SRC) $(PROTO_FILES) --go_out=$(PROTO_OUT) --go_opt=paths=source_relative --go-grpc_out=$(PROTO_OUT) --go-grpc_opt=paths=source_relative
--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,19 +1,31 @@
 package main
 
 import (
+	"context"
 	"github.com/OddEer0/mirage-auth-service/internal/infrastructure/config"
-	"github.com/go-chi/chi/v5"
-	"net/http"
+	authv1 "github.com/OddEer0/mirage-auth-service/pkg/gen/auth_v1"
+	"google.golang.org/grpc"
+	"net"
 )
+
+type AuthServiceServer struct {
+	authv1.UnimplementedAuthServiceServer
+}
+
+func (a *AuthServiceServer) Registration(ctx context.Context, request *authv1.RegistrationRequest) (*authv1.AuthResponse, error) {
+	//TODO implement me
+	panic("implement me")
+}
 
 func main() {
 	cfg := config.MustLoad()
-	router := chi.NewRouter()
-	router.Get("/", func(res http.ResponseWriter, req *http.Request) {
-		_, _ = res.Write([]byte("Hello"))
-	})
-	err := http.ListenAndServe(cfg.Server.Address, router)
+	lis, err := net.Listen("tcp", cfg.Server.Address)
 	if err != nil {
-		return
+		panic(err)
+	}
+	grpcServer := grpc.NewServer()
+	authv1.RegisterAuthServiceServer(grpcServer, &AuthServiceServer{})
+	if err := grpcServer.Serve(lis); err != nil {
+		panic(err)
 	}
 }
